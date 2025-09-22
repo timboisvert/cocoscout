6d883a162dd6ebb496beac379c46531b22b2db4d
Tim Boisvert <boisvert@gmail.com>
2025-09-22 11:36:48 -0500

    Audition form shows profile and allows update

--- app/controllers/respond_to_call_to_audition_controller.rb
+++ app/controllers/respond_to_call_to_audition_controller.rb
@@ -110,35 +110,21 @@ class RespondToCallToAuditionController < ApplicationController
       return
     end
 
+    @person = Current.user.person
+
     # First we'll check if they've already responded to this call to audition
-    if cookies.signed["#{@call_to_audition.hex_code}"]
+    if @call_to_audition.audition_requests.exists?(person: Current.user.person)
 
-      # The user has already responded, so look up their Person object and Audution Request
-      @person = Person.find_by(email: cookies.signed["#{@call_to_audition.hex_code}"])
-
-      if @person.nil?
-        cookies.delete "#{@call_to_audition.hex_code}"
-        redirect_to respond_to_call_to_audition_form_path(hex_code: @call_to_audition.hex_code), status: :see_other
-      else
-        @audition_request = @call_to_audition.audition_requests.find_by(person: @person)
-
-        if @audition_request.nil?
-          cookies.delete "#{@call_to_audition.hex_code}"
-          redirect_to respond_to_call_to_audition_form_path(hex_code: @call_to_audition.hex_code), status: :see_other
-        else
-          @answers = {}
-          @questions.each do |question|
-            answer = @audition_request.answers.find_by(question: question)
-            @answers["#{question.id}"] = answer.value if answer
-          end
-
-        end
+      @audition_request = @call_to_audition.audition_requests.find_by(person: Current.user.person)
+      @answers = {}
+      @questions.each do |question|
+        answer = @audition_request.answers.find_by(question: question)
+        @answers["#{question.id}"] = answer.value if answer
       end
 
     else
       # They haven't responded yet, so we'll let them create a new response
       @audition_request = AuditionRequest.new
-      @person = @audition_request.build_person
 
       # Empty answers hash
       @answers = {}
@@ -150,18 +136,13 @@ class RespondToCallToAuditionController < ApplicationController
   end
 
   def submitform
-    # Strong parameters for the person
-    person_params = params.require(:audition_request).permit(person: [ :stage_name, :email, :pronouns, :socials, :resume, :headshot, :questions ])
+    @person = Current.user.person
 
     # We may be updating an existing response, so check for that first
-    if cookies.signed["#{@call_to_audition.hex_code}"]
+    if @call_to_audition.audition_requests.exists?(person: Current.user.person)
 
       # Get the person and audition request
-      @person = Person.find_by(email: cookies.signed["#{@call_to_audition.hex_code}"])
       @audition_request = @call_to_audition.audition_requests.find_by(person: @person)
-
-      # Update the person with the new details
-      @person.assign_attributes(person_params[:person])
 
       # Update the answers
       @answers = {}
@@ -175,9 +156,8 @@ class RespondToCallToAuditionController < ApplicationController
     else
 
       # It's a new request, so instantiate the objects
-      @audition_request = AuditionRequest.new
+      @audition_request = AuditionRequest.new(person: @person)
       @audition_request.call_to_audition = @call_to_audition
-      @person = @audition_request.build_person(person_params[:person])
 
       # Loop through the questions and store the answers
       @answers = {}
@@ -190,48 +170,22 @@ class RespondToCallToAuditionController < ApplicationController
 
     end
 
-    if @audition_request.valid? && @person.valid?
+    if @audition_request.valid?
 
-      # Check to ensure this is a unique person for this call to audition
-      # If it's not unique, update the existing person
-      existing_person = Person.find_by(email: @person.email)
-      if existing_person
-        existing_person.assign_attributes(@person.attributes.except("id", "created_at", "updated_at"))
-        @person = existing_person
+      # Update the person with any updated details
+      person_params = params.require(:audition_request).permit(person: [ :stage_name, :pronouns, :socials, :resume, :headshot ])
+      @person.assign_attributes(person_params[:person])
 
-        # Add the headshot and resume if they've been passed in
-        @person.headshot = person_params[:person][:headshot] if person_params[:person][:headshot].present?
-        @person.resume = person_params[:person][:resume] if person_params[:person][:resume].present?
-
-        # Check to see if this person has already submitted an audition request for this call to audition
-        existing_audition_request = @call_to_audition.audition_requests.find_by(person: @person)
-        if existing_audition_request
-
-          # Update the existing audition request with any new details
-          existing_audition_request.assign_attributes(@audition_request.attributes.except("id", "created_at", "updated_at"))
-          @audition_request = existing_audition_request
-
-          # Put the new answers onto the existing audition request
-          new_answers = []
-          @answers.each do |answer|
-            new_answer = @audition_request.answers.build
-            new_answer.audition_request = @audition_request
-            new_answer.question = Question.find(answer.first)
-            new_answer.value = answer.last
-            new_answers << new_answer
-          end
-          @audition_request.answers = new_answers
-        end
-
-        # Make sure audition request points to the proper user if any changes have been made
-        @audition_request.person = @person
-
+      if @person.valid?
+        @person.save!
+      else
+        @person.reload
+        @update_person_error = true
+        render :form, status: :unprocessable_entity and return
       end
 
-      @person.save!
+      # Save the audition request and redirect to the success page
       @audition_request.save!
-
-      cookies.signed["#{@call_to_audition.hex_code}"] = { value: @person.email, expires: 5.years.from_now }
       redirect_to respond_to_call_to_audition_success_path(hex_code: @call_to_audition.hex_code), status: :see_other
     else
       render :form
