7ac4b6e6084c70a9c37b7379d6ce2e2306a9492e
Tim Boisvert <boisvert@gmail.com>
2025-09-22 17:11:07 -0500

    Update

--- db/migrate/20250922140000_add_invitation_fields_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddInvitationFieldsToUsers < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :invitation_code, :string
-    add_column :users, :invitation_status, :string
-  end
-end
