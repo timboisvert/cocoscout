3358859162ae2bd283591bc5b1e98736fa13b077
Tim Boisvert <boisvert@gmail.com>
2025-09-27 09:23:03 -0500

    profile tweaks

--- app/controllers/my/shows_controller.rb
+++ app/controllers/my/shows_controller.rb
@@ -2,4 +2,15 @@ class My::ShowsController < ApplicationController
   def index
     @productions = Production.joins(casts: [ :casts_people ]).where(casts_people: { person_id: Current.user.person.id }).distinct
   end
+
+  def show
+    @production = Production.find(params[:id])
+    @shows = @production.shows.order(date_and_time: :asc)
+
+    # Get my assignments, then relate them to each show
+    show_person_role_assignments = @production.show_person_role_assignments.where(show_id: @shows.pluck(:id), person_id: Current.user.person.id)
+    @assignments_by_show = @shows.index_with do |show|
+      show_person_role_assignments.find { |assignment| assignment.show_id == show.id }
+    end
+  end
 end
--- app/models/production.rb
+++ app/models/production.rb
@@ -1,11 +1,12 @@
 class Production < ApplicationRecord
+    has_many :posters, dependent: :destroy
     has_many :shows, dependent: :destroy
     has_many :call_to_auditions, dependent: :destroy
     has_many :audition_requests, through: :call_to_auditions
     has_many :audition_sessions, dependent: :destroy
     has_many :casts, dependent: :destroy
     has_many :roles, dependent: :destroy
-    has_many :posters, dependent: :destroy
+    has_many :show_person_role_assignments, through: :shows
     belongs_to :production_company
 
     has_one_attached :logo, dependent: :purge_later do |attachable|
