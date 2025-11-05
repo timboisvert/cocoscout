47d0f58d49be828dfd72b41f4ee945438426033c
Tim Boisvert <boisvert@gmail.com>
2025-11-05 10:56:49 -0600

    Fixed rubocop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 
 # Associate all people with production company 1 if it's namedCoco Runs Everything"
 production_company = ProductionCompany.find_by(id: 1)
-if production_company && production_company.name ==Coco Runs Everything"
+if production_company && production_company.name == "Coco Runs Everything"
   Person.find_each do |person|
     unless person.production_companies.include?(production_company)
       person.production_companies << production_company
-      putsAdded #{person.name} to #{production_company.name}"
+      putsAdded # {person.name} to #{production_company.name}"
     end
   end
 end
@@ -16,4 +16,4 @@ Person.where(user_id: nil).each do |person|
   next if productions.empty?
   people << person.email
 end
-people
+puts people
