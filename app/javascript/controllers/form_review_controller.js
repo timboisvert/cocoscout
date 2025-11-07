1683eaf529c963c9486e53ca0247ac16d95e2646
Tim Boisvert <boisvert@gmail.com>
2025-11-07 15:29:57 -0600

    Remove scheduled sessions

--- app/javascript/controllers/form_review_controller.js
+++ app/javascript/controllers/form_review_controller.js
@@ -1,24 +1,22 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["checkbox", "submitButton"]
+    static targets = ["checkbox", "submitButton"]
 
-  connect() {
-    // Hide button by default if checkbox is unchecked
-    this.updateButtonVisibility()
-  }
-
-  toggleSubmitButton() {
-    this.updateButtonVisibility()
-  }
-
-  updateButtonVisibility() {
-    if (this.checkboxTarget.checked) {
-      this.submitButtonTarget.classList.remove("hidden")
-      this.submitButtonTarget.classList.add("inline-block")
-    } else {
-      this.submitButtonTarget.classList.add("hidden")
-      this.submitButtonTarget.classList.remove("inline-block")
+    connect() {
+        // Hide button by default if checkbox is unchecked
+        this.updateButtonVisibility()
     }
-  }
+
+    toggleSubmitButton() {
+        this.updateButtonVisibility()
+    }
+
+    updateButtonVisibility() {
+        if (this.checkboxTarget.checked) {
+            this.submitButtonTarget.style.display = "inline-block"
+        } else {
+            this.submitButtonTarget.style.display = "none"
+        }
+    }
 }
