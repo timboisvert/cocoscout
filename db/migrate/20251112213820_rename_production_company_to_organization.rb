d15e7d21fa2e986ad28fcf1ec4a41a23967420a0
Tim Boisvert <boisvert@gmail.com>
2025-11-12 18:09:36 -0600

    Getting to the last production sisues

--- db/migrate/20251112213820_rename_production_company_to_organization.rb
+++ db/migrate/20251112213820_rename_production_company_to_organization.rb
@@ -3,15 +3,34 @@ class RenameProductionCompanyToOrganization < ActiveRecord::Migration[8.1]
     # Rename the main table
     rename_table :production_companies, :organizations
 
-    # Rename foreign key columns
-    rename_column :productions, :production_company_id, :organization_id
-    rename_column :user_roles, :production_company_id, :organization_id
-    rename_column :invitations, :production_company_id, :organization_id
-    rename_column :person_invitations, :production_company_id, :organization_id
-    rename_column :locations, :production_company_id, :organization_id
+    # Rename foreign key columns (with safety checks)
+    if column_exists?(:productions, :production_company_id)
+      rename_column :productions, :production_company_id, :organization_id
+    end
+
+    if column_exists?(:user_roles, :production_company_id)
+      rename_column :user_roles, :production_company_id, :organization_id
+    end
+
+    if table_exists?(:invitations) && column_exists?(:invitations, :production_company_id)
+      rename_column :invitations, :production_company_id, :organization_id
+    end
+
+    if column_exists?(:person_invitations, :production_company_id)
+      rename_column :person_invitations, :production_company_id, :organization_id
+    end
+
+    if column_exists?(:locations, :production_company_id)
+      rename_column :locations, :production_company_id, :organization_id
+    end
 
     # Rename join table
-    rename_table :people_production_companies, :organizations_people
-    rename_column :organizations_people, :production_company_id, :organization_id
+    if table_exists?(:people_production_companies)
+      rename_table :people_production_companies, :organizations_people
+    end
+
+    if table_exists?(:organizations_people) && column_exists?(:organizations_people, :production_company_id)
+      rename_column :organizations_people, :production_company_id, :organization_id
+    end
   end
 end
