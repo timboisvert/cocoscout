328eb6fc86d8f6d568b10fd10e0c5598ebd41c90
Tim Boisvert <boisvert@gmail.com>
2025-11-21 14:20:31 -0600

    Fix bum migration

--- db/migrate/20251120184037_rename_casts_to_talent_pools.rb
+++ db/migrate/20251120184037_rename_casts_to_talent_pools.rb
@@ -7,11 +7,8 @@ class RenameCastsToTalentPools < ActiveRecord::Migration[8.1]
     rename_table :casts_people, :people_talent_pools
 
     # Rename foreign key columns in join table
+    # Note: Rails automatically renames indexes when you rename columns
     rename_column :people_talent_pools, :cast_id, :talent_pool_id
-
-    # Rename index on the join table
-    remove_index :people_talent_pools, name: "index_people_talent_pools_on_cast_id"
-    add_index :people_talent_pools, :talent_pool_id, name: "index_people_talent_pools_on_talent_pool_id"
 
     # Rename columns in other tables that reference casts
     rename_column :cast_assignment_stages, :cast_id, :talent_pool_id
