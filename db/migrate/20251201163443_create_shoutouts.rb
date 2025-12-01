dd134b53fd397001f13fd9d9d6951bea47fe26ef
Tim Boisvert <boisvert@gmail.com>
2025-12-01 16:07:45 -0600

    fix rubocop issues

--- db/migrate/20251201163443_create_shoutouts.rb
+++ db/migrate/20251201163443_create_shoutouts.rb
@@ -14,7 +14,7 @@ class CreateShoutouts < ActiveRecord::Migration[8.1]
     end
 
     # Index for efficient querying of shoutouts for a specific entity
-    add_index :shoutouts, [:shoutee_type, :shoutee_id, :created_at], name: "index_shoutouts_on_shoutee_and_created"
-    add_index :shoutouts, [:author_id, :created_at], name: "index_shoutouts_on_author_and_created"
+    add_index :shoutouts, [ :shoutee_type, :shoutee_id, :created_at ], name: "index_shoutouts_on_shoutee_and_created"
+    add_index :shoutouts, [ :author_id, :created_at ], name: "index_shoutouts_on_author_and_created"
   end
 end
