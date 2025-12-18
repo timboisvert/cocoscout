071e568244663bcf16214b1478ef83a53bdede7f
Tim Boisvert <boisvert@gmail.com>
2025-12-17 20:31:28 -0600

    role eligibilities cleanup

--- db/migrate/20251218011027_remove_bad_role_eligibilities_index.rb
+++ db/migrate/20251218011027_remove_bad_role_eligibilities_index.rb
@@ -2,7 +2,7 @@ class RemoveBadRoleEligibilitiesIndex < ActiveRecord::Migration[8.1]
   def change
     # The old index on (role_id, member_id) doesn't include member_type,
     # which breaks polymorphic uniqueness (Person#1 vs Group#1 would conflict)
-    remove_index :role_eligibilities, [:role_id, :member_id], 
+    remove_index :role_eligibilities, [ :role_id, :member_id ],
                  name: "index_role_eligibilities_on_role_id_and_member_id",
                  if_exists: true
   end
