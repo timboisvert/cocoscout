7db2bae4fa3c53cc9c349c941970a8d70a8aaad8
Tim Boisvert <boisvert@gmail.com>
2026-03-06 14:38:48 -0600

    demo org

--- db/migrate/20260306120000_add_is_demo_to_organizations.rb
+++ db/migrate/20260306120000_add_is_demo_to_organizations.rb
@@ -1,5 +1,13 @@
 class AddIsDemoToOrganizations < ActiveRecord::Migration[8.1]
   def change
     add_column :organizations, :is_demo, :boolean, default: false, null: false
+
+    reversible do |dir|
+      dir.up do
+        execute <<-SQL
+          UPDATE organizations SET is_demo = true WHERE name LIKE '%(Demo%' OR name LIKE '%(Demo)%'
+        SQL
+      end
+    end
   end
 end
